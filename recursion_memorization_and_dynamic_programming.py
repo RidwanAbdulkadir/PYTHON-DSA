38199187b461e3bc44d138fab3451e6d6207df44
RidwanAbdulkadir <olayinkaridwan20@gmail.com>
2026-03-11 12:40:04 -0400

    "update"

--- recursion_memorization_and_dynamic_programming.py
+++ recursion_memorization_and_dynamic_programming.py
@@ -1,5 +1,10 @@
 '''
 RECURSION, MEMORIZATION AND DYNAMIC PROGRAMMING
 
-Recursion is
+Recursion is a programming technique where a function calls itself in order to solve a problem. It typically involves a base case that stops the recursion and a recursive case that breaks the problem into smaller subproblems.
+
+Memorization is an optimization technique used to speed up recursive algorithms by storing the results of expensive function calls and returning the cached result when the same inputs occur again. This can significantly reduce the time complexity of recursive algorithms.
+
+Dynamic programming is a method for solving complex problems by breaking them down into simpler subproblems, storing the results of those subproblems to avoid redundant work. It can be implemented using either a top-down approach (with recursion and memorization) or a bottom-up approach (iteratively filling a table).
 '''
+
